37268a43bb67100ce5444f0c72ebc69eecdc7260
myokss <myokkeu@gmail.com>
2024-07-30 20:17:01 +0800

    added upload modal

--- resources/views/admin/reviewer.blade.php
+++ resources/views/admin/reviewer.blade.php
@@ -10,8 +10,35 @@
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-5">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 pb-2 mt-3 mb-3">
                     <h1 class="h3 fw-semibold">Reviewer</h1>
-                    <div class="btn-toolbar mb-2 mb-md-0">
-                        <button type="button" class="btn btn-primary btn-md" style="font-size: 0.9rem; padding: 0.8em 1em">Upload Materials</button>
+                    <div class="btn-toolbar mb-2 mb-md-0 ">
+                        <button type="button" class="btn btn-primary btn-md" data-bs-toggle="modal" data-bs-target="#uploadModal"
+                            style="font-size: 0.9rem; padding: 0.8em 1em">Upload Materials</button>
+                    </div>
+                </div>
+
+                <!-- Modal Structure -->
+                <div class="modal fade" id="uploadModal" tabindex="-1" aria-labelledby="uploadModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="uploadModalLabel">Upload Materials</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="materialFile" class="form-label fs-6">Select file to upload</label>
+                                        <input type="file" class="form-control" id="materialFile">
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" style="font-size: 0.9rem; padding: 0.7em 1em" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" style="font-size: 0.9rem; padding: 0.7em 1em" >Upload</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -33,7 +60,8 @@
                                     </p>
                                     <h6 class="card-title mb-2" style="font-size: 1rem;">{{ $file['name'] }}</h6>
 
-                                    <span class="badge bg-success " style="font-size: 0.65rem;font-weight: normal; padding: 0.6em 1em;">Success</span>
+                                    <span class="badge bg-success "
+                                        style="font-size: 0.65rem;font-weight: normal; padding: 0.6em 1em;">Success</span>
                                 </div>
                             </div>
                         </div>
@@ -42,5 +70,5 @@
             </main>
         </div>
     </div>
-  
+
 @endsection

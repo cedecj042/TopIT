481a5553517567c7a1e6316733f488fd33627191
Cedric Oporto <cedricoporto1st@gmail.com>
2024-07-31 14:54:26 +0800

    setting up the chartjs, and others

--- resources/views/student/shared/student-navbar.blade.php
+++ resources/views/student/shared/student-navbar.blade.php
@@ -1,11 +1,16 @@
 <div class="col-12">
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                
-                <img src="{{Auth::}}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                
-            </a>
+    <nav class="navbar navbar-custom d-flex justify-content-end">
+        <div class="d-flex flex-row pe-5 py-2 gap-3 ">
+            <span class="ms-2">Hi, {{ Auth::user()->username }}</span>
+            @php
+                if (Auth::user()->userable->profile_image == null) {
+                    $profileImageUrl = asset('assets/profile-circle.png');
+                } else {
+                    $profileImageUrl = Storage::disk('profile_images')->url(Auth::user()->userable->profile_image);
+                    //$profileImageUrl = Storage::url('profile_images/'.Auth::user()->userable->profile_image);
+                }
+            @endphp
+            <img src="{{ $profileImageUrl }}" alt="Profile Image" class="rounded-circle" width="30" height="30">
         </div>
     </nav>
 </div>
--- resources/views/student/ui/dashboard.blade.php
+++ resources/views/student/ui/dashboard.blade.php
@@ -3,16 +3,25 @@
 @section('title', 'Dashboard')
 
 @section('page-content')
-    <div class="container-fluid">
-        <div class="row">
-            @include('student.shared.student-sidebar')
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-5">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 pb-2 mt-3 mb-3">
-                <h1 class="h3 fw-semibold">Dashboard</h1>
+<div class="container-fluid">
+    <div class="row">
+        @include('student.shared.student-sidebar')
+        <main class="col-md-9 ms-sm-auto col-lg-10 p-0">
+            @include('student.shared.student-navbar')
+            <div class="row p-3">
+                <div class="d-flex justify-content-between flex-column flex-wrap flex-md-nowrap align-items-start p-5 mb-3">
+                    <h3 class="fw-bold">Dashboard</h3>
+                    <div class="row">
+                        <h5>Topics</h5>
+                        <canvas id="myChart" width="300" height="200"></canvas>
+                    </div>
                 </div>
 
-        
-            </main>
-        </div>
+            </div>
+
+
+
+        </main>
     </div>
+</div>
 @endsection
